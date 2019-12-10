23dcf27e2adcacf2a3903948f4b39989beb1a087
sunnykim91 <tjsgnl2002@gmail.com>
2019-12-10 16:08:34 +0900

    init setting css about 40%

--- src/App.js
+++ src/App.js
@@ -1,76 +1,15 @@
-import React, { Component } from 'react';
-import BodyArea from './components/common/BodyArea'
-import NavBar from './components/common/NavBar'
-import LoginLeft from './components/login/LoginLeft';
-import LoginRight from './components/login/LoginRight';
-import './components/common/BodyArea.css';
-import TwoSplitContent from './components/common/TwoSplitContent';
-import './components/common/TwoSplitContent.css';
-import OrderList from './components/worship/OrderList';
-import AdList from './components/worship/AdList';
-import Helper from './components/worship/Helper';
-import Axios from 'axios';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Main from './components/main/Main';
+import DetailWorship from './components/worship/DetailWorship';
 
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pageName: 'update',
-      showNav:'true',
-      serverUrl : 'http://aaaicu.synology.me:8088/OhJooYeoMVC',
-      orderContents:[]
-    }
-  }
-  render() {
-    let bodyContent =[]
-    const { pageName} = this.state;
-    let orderContents = [];
-    let adContents = [];
-    switch (pageName) {
-      case 'login':
-        bodyContent =
-          [<LoginLeft />,
-          <LoginRight />];
-          break;
-
-      case 'add':
-        orderContents = ["tttt","ssssss","zzzzzz","tttt","ssssss","zzzzzz","tttt","ssssss","zzzzzz",  "tttt","ssssss","zzzzzz"];
-        adContents = ["tttt","ssssss","zzzzzz","tttt","ssssss","zzzzzz","tttt","ssssss","zzzzzz",  "tttt","ssssss","zzzzzz"];
-        bodyContent =
-        [<TwoSplitContent key="add" mode={pageName} left={[(<Helper />),(<OrderList orderContents = {orderContents}/>)]} right={[(<AdList  adContents = {adContents}/>)]}/>];
-        break;
-
-      case 'update':
-
-        Axios.post('http://localhost:8080/ohjooyeo/worship/info/',{
-          "churchId": "1",
-          "worshipId" : "19-001",
-          "version" : 0
-        }).then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-        });
-
-        orderContents = ["tttt","ssssss","zzzzzz","tttt","ssssss","zzzzzz","tttt","ssssss","zzzzzz",  "tttt","ssssss","zzzzzz"];
-        adContents = ["tttt","ssssss","zzzzzz","tttt","ssssss","zzzzzz","tttt","ssssss","zzzzzz",  "tttt","ssssss","zzzzzz"]; 
-        
-        bodyContent =
-        [<TwoSplitContent key="update" mode={pageName} left={[(<Helper key={"left"+0}/>),(<OrderList key={"left"+1} orderContents = {orderContents}/>)]} right={[(<AdList key={"right"+0} adContents = {adContents}/>)]}/>];
-        break;  
-
-      default: bodyContent=(<div>default Page</div>)
-    }
-    return (
-      <div>
-        <BodyArea>
-          <NavBar showNav ={this.state.showNav}/>
-          {bodyContent}
-        </BodyArea>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Route path='/' component={Main} exact={true} />
+      <Route path='/detailWorship/:id' component={DetailWorship} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/main/Main.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import '../../reset.css';
+import './Main.css';
+
+const Main = () => {
+  const [juboList, setJuboList] = useState([]);
+
+  const getJuboList = async () => {
+    const baseURL = 'http://aaaicu.synology.me:8088/OhJooYeoMVC';
+    Axios.post(`${baseURL}/worship/list/`, {
+      'churchId': 1
+    })
+      .then(response => {
+        console.log(response.data);
+        setJuboList(response.data);
+        console.log(juboList);
+        // for (let i = 0; i < juboList.length; i++) {
+        //   Axios.post(`${baseURL}/worship/info`, {
+        //     'churchId': 1,
+        //     'worshipId': `${response.data[i].worshipId}`,
+        //     'version': 0
+        //   }).then(response => {
+        //     console.log(response);
+        //   });
+        // }
+      })
+      .then(() => {
+        console.log(juboList);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getJuboList();
+  }, []);
+
+  return (
+    <Container>
+      <div className='dataFilter'>
+        <Fab color='primary' aria-label='add'>
+          <ChevronLeftIcon />
+        </Fab>
+        <div className='todayMonth'>11월</div>
+        <Fab color='secondary' aria-label='edit'>
+          <ChevronRightIcon />
+        </Fab>
+      </div>
+      <div className='juboList'>
+        {juboList.map(jubo => (
+          <Link to={`/detailWorship/${jubo.worshipId}`}>
+            <div className='JuboList_item' key={jubo.worshipId}>
+              <Card>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant='h5' component='h2'>
+                      {jubo.date}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Main;
